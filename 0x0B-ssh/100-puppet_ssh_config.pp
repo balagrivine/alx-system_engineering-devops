e4a604a4e324f91749780ef1f25d9e29663847b9
balagrivine <balagrivine@gmail.com>
2024-01-21 12:06:21 -0800

    using puppet to make changes to the default ssh config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#client-side configuration to enable authentication without password
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentifyFile ~/.ssh/school',
+  replace => true,
+}
