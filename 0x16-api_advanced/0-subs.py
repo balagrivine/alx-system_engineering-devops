2d9e6b073a554e330e36898148bd35626a8757ca
balagrivine <balagrivine@gmail.com>
2024-03-05 09:41:13 -0800

    make code pycodestyle compliant

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 
-"""function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+"""function that queries the Reddit API
+and returns the number of subscribers
+(not active users, total subscribers) for
+a given subreddit. If an invalid subreddit
+is given, the function should return 0.
 """
+
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit"""
+    """queries the Reddit API and returns the
+    number of subscribers (not active users,
+    total subscribers) for a given subreddit"""
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
@@ -16,7 +24,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data =  response.json().get('data', {})
+        data = response.json().get('data', {})
         sub_count = data.get('subscribers', 0)
 
         return sub_count
